b9852be9e47f86f1c4b61bd0b8ee2e28df99ad79
Leonardo Durante <leofdurante@gmail.com>
2026-03-06 01:39:34 -0700

    Update server port configuration to use environment variable

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const app = require('./service.js');
 
-const port = process.argv[2] || 3000;
+const port = process.env.PORT || 80;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
